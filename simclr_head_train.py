fbf79ca30b96c14ee56734678e0b745c05d2d8fd
Ferdinand John Briones <fjbriones@outlook.com>
2021-06-30 15:38:38 +0800

    Moved byol code within repo

--- simclr_head_train.py
+++ simclr_head_train.py
@@ -191,7 +191,8 @@ def train(opt):
         text, length = converter.encode(labels, batch_max_length=opt.batch_max_length)
         batch_size = image.size(0)
 
-        feature = model(image, is_train=False)
+        with torch.no_grad():
+            feature = model(image, is_train=False)
         # feature = feature.view(-1, 26, feature.shape[1])
 
         if opt.Prediction == "CTC":
